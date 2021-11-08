443210ac98882c0ed271f9e7b51dfcf4cded3ee7
Ruben <rubendomingo.dam@ciudadescolarfp.es>
2021-11-08 11:56:48 +0100

    fd

--- aadu2_Ruben_DomingoRobledo/src/main/java/com/add/ejercicio2/HikariCP.java
+++ aadu2_Ruben_DomingoRobledo/src/main/java/com/add/ejercicio2/HikariCP.java
@@ -1,5 +1,25 @@
 package com.add.ejercicio2;
 
-public class HikariCP {
+import com.zaxxer.hikari.HikariConfig;
 
+public class HikariCP {
+    
+    private static HikariConfig config = new HikariConfig();
+    private static HikariDataSource ds;
+    
+    static {
+        config.setJdbcUrl("jdbc:h2:mem:test");
+        config.setUsername("user");
+        config.setPassword("password");
+        config.addDataSourceProperty("cachePrepStmts", "true");
+        config.addDataSourceProperty("prepStmtCacheSize", "250");
+        config.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
+        ds = new HikariDataSource(config);
+    }
+    
+    public static Connection getConnection() throws SQLException {
+        return ds.getConnection();
+    }
+    
+    private HikariCP(){}
 }
